2c502d29229cf328f8e7e6900888858d40504e50
aliyarar <a.yrr@outlook.com>
2021-03-27 18:54:45 +0100

    create,read and write

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,47 @@
 <template>
   <div>
-   <b-button v-b-modal.modal-1>Launch demo modal</b-button>
-
-    <b-modal id="modal-1" title="BootstrapVue">
-      <p class="my-4">Hello from modal!</p>
-    </b-modal>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="4">
+          <AddRoleModel @fromAddRoleModel="updateCard($event)"/>
+        </b-col>
+        <b-col sm="8"> 
+          <ModelCard v-for="(item,index) in card" 
+            :key="index" 
+            :index="index" 
+            :modelName="item[0]" 
+            :modelJob="item[1]" 
+            :modelNationality="item[2]"
+            :modelBirthDay="item[3]" 
+            :modelAbout="item[4]" 
+            :modelPic="item[5]" 
+            :tags="item[6]" />
+          </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
+
 <script>
-
-
+import globalComponenets from "./components/globalComponents"
 export default {
-  name: 'App',
-  
-}
+  components: {
+    ...globalComponenets
+  },
+  data(){
+    return{
+      card:[]
+    }
+  },
+  methods:{
+    updateCard(e){
+      this.card.push(e);
+      console.log(e);
+    }
+  }
+
+};
 </script>
 
-<style>
 
-</style>
---
+++ src/components/globalComponents.js
@@ -0,0 +1,7 @@
+import AddRoleModel from "./AddRoleModel"
+import ModelCard from "./ModelCard"
+
+export default{
+    ModelCard,
+    AddRoleModel
+}
